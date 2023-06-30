eb103a40a434d5a665b6b515c3d2af11e324206e
Lucia <122565245+ll5zh@users.noreply.github.com>
2023-06-30 09:06:17 -0400

    Add button click interface

--- script.js
+++ script.js
@@ -32,3 +32,39 @@ function operate(n1, op, n2) {
         divide(n1, n2);
     }
 }
+
+const topBtns = document.querySelectorAll('.top');
+topBtns.forEach(function(currentValue) {
+    currentValue.addEventListener('mousedown', function(e) {
+        currentValue.style.cssText = 'background-color: rgb(255, 194, 141)';
+    });
+});
+topBtns.forEach(function(currentValue) {
+    currentValue.addEventListener('mouseup', function(e) {
+        currentValue.style.cssText = 'background-color: peachpuff';
+    });
+});
+
+const numBtns = document.querySelectorAll('.num');
+numBtns.forEach(function(currentValue) {
+    currentValue.addEventListener('mousedown', function(e) {
+        currentValue.style.cssText = 'background-color: rgb(110, 234, 110)';
+    })
+});
+numBtns.forEach(function(currentValue) {
+    currentValue.addEventListener('mouseup', function(e) {
+        currentValue.style.cssText = 'background-color: rgb(162, 250, 162)';
+    })
+});
+
+const opBtns = document.querySelectorAll('.op');
+opBtns.forEach(function(currentValue) {
+    currentValue.addEventListener('mousedown', function(e) {
+        currentValue.style.cssText = 'background-color: rgb(255, 166, 181)';
+    });
+});
+opBtns.forEach(function(currentValue) {
+    currentValue.addEventListener('mouseup', function(e) {
+        currentValue.style.cssText = 'background-color: rgb(251, 201, 209)';
+    });
+});
